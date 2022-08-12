f20c893195873c0f035292784934f0eab810d00e
ilovedeo <nonlunch@gmail.com>
2022-08-12 17:34:13 +0900

    revised parsing code

--- parsing.py
+++ parsing.py
@@ -3,6 +3,7 @@
 import pandas as pd
 from datetime import datetime
 from train import *
+import numpy as np
 
 # here: current directory.
 here = os.getcwd()
@@ -27,62 +28,93 @@ rename_file(download_path)
 # encoded value of noise level and semanticity
 #################################################################################
 
-### Data import module
+### Data import and store module.
+# First make directory to store data
+parsed_dir = os.path.join(download_path + "/episode_parsed")
+if not os.path.isdir(parsed_dir):
+    os.mkdir(parsed_dir)
 
 # Example : we will do this in iterative way using for loop.
 # for i in dir...
-i = 1
+i = 0
+subject = i + 1
 # Signal directory.
 data_dir = os.path.join(
     download_path
     + "/phyaat_dataset"
     + "/Signals"
-    + "/S{0:02d}".format(i)
-    + "/S{}_Signals.csv".format(i)
+    + "/S{0:02d}".format(subject)
+    + "/S{}_Signals.csv".format(subject)
 )
 # Score directory.
 score_dir = os.path.join(
     download_path
     + "/phyaat_dataset"
     + "/Signals"
-    + "/S{0:02d}".format(i)
-    + "/S{}_Textscore.csv".format(i)
+    + "/S{0:02d}".format(subject)
+    + "/S{}_Textscore.csv".format(subject)
 )
 
-# Define datetime format.
-dt_parser = lambda x: datetime.strptime(x, "%H:%M:%S:%f")
+# Make destination directory.
+subject_dir = os.path.join(parsed_dir + "/S{0:02d}".format(subject))
+if not os.path.isdir(subject_dir):
+    os.mkdir(subject_dir)
+
+# Define columns to reject TimeStamp.
+data_cols = list(pd.read_csv(data_dir, nrows=1))
+score_cols = list(pd.read_csv(score_dir, nrows=1))
 
 # Read signal file by panda: set y/m as 1900/01/01.
 data = pd.read_csv(
-    data_dir, sep=",", parse_dates=["TimeStamp"], date_parser=dt_parser
+    data_dir, sep=",", usecols=[i for i in data_cols if i != "TimeStamp"]
 )
 
 score = pd.read_csv(
-    score_dir, sep=",", parse_dates=["TimeStamp"], date_parser=dt_parser
+    score_dir, sep=",", usecols=[i for i in score_cols if i != "TimeStamp"]
 )
 
+###
+# Create discontinuous interval list.
+intervals = find_disCT(data, "Label_T")
+
+###
+# Add score:
+# Create dataframe with 3 columns.
 task_list = []
+# Add "TotalW", "CorrectWords"
+new_index = ["TotalW", "CorrectWords"]
+add_data = pd.dataframe()
+
 
 ###
-# Store header.
-if i == 1:
-    cols = list(pd.read_csv(data_dir, nrows =1))
+# Define a mapper function.
+def mapper(data, score, intervals, new_index):
+    for i, itv in enumerate(intervals):
+        store = data.loc[itv]
+        # Initial experiment condition
+        if i == 0:
+
+
+        else:
+            for col_name in new_index:
+                dataframe[col_name] = val_list[i]
+
+    return data
+
+###
+# And then store header for first subject.
+if subject == 1:
+    cols = list(pd.read_csv(data_dir, nrows=1))
     task_list += [cols]
+
 ###
-
-
-
 # Remove -1 and add to list.
-
-
+rest_index = list(data[data["Label_T"] < 0].index)
+des_data, sep_data = separator(data, rest_index, axis=0)
 
 ###
-
 # Create unique label for each problems // concat TotalW, CorrectW, and dont use score file.
 
 # Find index where problem ends.
 
-# Discontinuity finder function.
-def find_disCT(series):
 
-    return True
--- train/utils.py
+++ train/utils.py
@@ -8,6 +8,7 @@ import phyaat
 # download_path: ./attention_entropy/train/datasets/data
 def rename_file(download_path):
     if not os.path.isdir(download_path):
+        os.mkdir(download_path)
         dirPath = phyaat.download_data(
             baseDir=download_path, subject=-1, verbose=0, overwrite=False
         )
@@ -24,11 +25,58 @@ def rename_file(download_path):
             dst_filename = os.path.join(
                 data_path + "/" + dir[0] + "{0:02d}".format(int(dir[1:]))
             )
-            print("Original filename")
+            print("Original filename: ")
             print(org_filename)
-            print("Revised filename")
+            print("Revised filename: ")
             print(dst_filename)
             os.rename(org_filename, dst_filename)
+
+
+# A function that receive column name, index list and
+# separate original data and designated indexed data.
+def separator(orig_data, index_list, axis=0):
+    des_data = orig_data.loc[index_list]
+    sep_data = orig_data.drop(index_list, axis=axis)
+    return des_data, sep_data
+
+
+# Discontinuity finder function:
+# returns a chunk of index list // separated by discontinuity.
+def find_disCT(data, col_name):
+    # Initial conditions
+    # A set of intervals
+    disCT_list = []
+    # Subintervals
+    interval = []
+    function = data[col_name].to_numpy()
+    # Start value with first value of the function.
+    value = function[0]
+    for i in range(len(function)):
+        if function[i] == value:
+            interval += [i]
+        else:
+            disCT_list += [interval]
+            interval = [i]
+            value = function[i]
+
+    return disCT_list
+
+
+def data_checker(data, col_name):
+    # Initial conditions
+    # A set of intervals
+    disCT_list = []
+    function = data[col_name].to_numpy()
+    # Start value with first value of the function.
+    value = False
+    for i in range(len(function)):
+        if function[i] == value:
+            continue
+        else:
+            disCT_list += [function[i]]
+            value = function[i]
+
+    return disCT_list
 
 
 # Needs to be revised...
